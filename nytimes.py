b1953a500d95247e6eb4a313bbbb197632bcbc10
Milton Pagan <milton.pagansoto@gmail.com>
2025-04-11 16:58:08 -0500

    feat: complete rewrite

--- nytimes.py
+++ nytimes.py
@@ -1,59 +1,39 @@
+import requests as r
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
 from time import sleep
 
-# TPL card number and pin, Gmail and password
-CARDNUM = ''
-CARDPIN = ''
-EMAIL = ''
-PASSWORD = ''
+# NYT credentials
+EMAIL = 'milton.pagansoto@gmail.com'
+PASSWORD = 'ychwRJ6VHkQMMteaVDo7'
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+wait = WebDriverWait(driver, 10)  # more realistic wait time
 
-# Access TPL site for NYT access
-driver.get('https://www.torontopubliclibrary.ca/detail.jsp?Entt=RDMEDB0202&R=EDB0202')
-driver.find_element_by_css_selector('.button.access-online.clear-left').click()
+# Access Redeem site for NYT access
+driver.get('https://www.nytimes.com/subscription/redeem?campaignId=8LRFH&gift_code=eb8412d177990b25')
+sleep(2)
 
-# Log in to TPL
-driver.find_element_by_id('userId').send_keys(CARDNUM)
-pin = driver.find_element_by_id('password')
-pin.send_keys(CARDPIN)
-pin.submit()
+# Close popup if it appears
+if driver.find_elements(By.CSS_SELECTOR, '.css-hqisq1'):
+    driver.find_element(By.CSS_SELECTOR, '.css-17rpywy').click()
 
-driver.find_element_by_css_selector('.button.submit.access-online').click()
-driver.find_element_by_partial_link_text('Log in here').click()
+# Log in to NYT
+email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#email')))
+email.send_keys(EMAIL)
 
-# Save current window because there will be a popup
-main_page = driver.current_window_handle
+password = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#password')))
+password.send_keys(PASSWORD)
 
-# Authenticate with Google instead of NYT account to avoid captcha
-driver.find_element_by_id('js-google-oauth-login').click()
+login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.css-1i3jzoq-buttonBox-buttonBox-primaryButton-primaryButton-Button')))
+login_button.click()
 
-sleep(1.5)
+continue_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.css-t0f7tz')))
+continue_button.click()
 
-# Switch to popup
-for handle in driver.window_handles:
-    if handle != main_page:
-        login_page = handle
-        break
-driver.switch_to.window(login_page)
-
-# Log in to Google
-driver.find_element_by_id('identifierId').send_keys(EMAIL)
-driver.find_element_by_css_selector('.RveJvd.snByac').click()
-sleep(1)
-driver.find_element_by_name('password').send_keys(PASSWORD)
-driver.find_element_by_css_selector('.RveJvd.snByac').click()
-
-# Switch back to main page
-driver.switch_to.window(main_page)
-
-# Wait for authentication to go through, then quit
-while True:
-    links = driver.find_elements_by_link_text('Go to NYTimes.com')
-    if len(links) == 0:
-        sleep(0.25)
-    else:
-        sleep(0.1)
-        driver.quit()
-        break
-
+sleep(0.1)
+driver.quit()
